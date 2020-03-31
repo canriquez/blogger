03a251b82e32b5596c851f937f2ef0e4a4a5557f
Carlos Anriquez <canriquez@gmail.com>
2020-03-31 18:06:05 -0300

    finishing updte controller and views. including flash info updates

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,6 +16,7 @@ class ArticlesController < ApplicationController
     def create
       @article = Article.new(article_params)
       @article.save
+      flash.notice = "Article '#{@article.title}' Created!"
       redirect_to article_path(@article)
 
     end
@@ -23,6 +24,7 @@ class ArticlesController < ApplicationController
     def destroy
         @article = Article.find(params[:id])
         @article.destroy
+        flash.notice = "Article '#{@article.title}' destroyed!"
         redirect_to articles_path
     end
 
@@ -31,6 +33,12 @@ class ArticlesController < ApplicationController
     end
 
     def update
+        @article = Article.find(params[:id])
+        @article.update(article_params)
+
+        flash.notice = "Article '#{@article.title}' Updated!"
+
+        redirect_to article_path(@article)
     end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   resources :articles
+
+  root to: 'articles#index'
+
+
 end
