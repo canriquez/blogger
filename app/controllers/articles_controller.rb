709e3b956e0f6955fcd91555eb26ecea2583580a
Carlos Anriquez <canriquez@gmail.com>
2020-03-31 17:29:03 -0300

    finished destroy method controller and methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+    include ArticlesHelper
 
     def index
         @articles = Article.all
@@ -13,12 +14,23 @@ class ArticlesController < ApplicationController
     end
 
     def create
-      @article = Article.new(
-        title: params[:article][:title],
-        body: params[:article][:body])
+      @article = Article.new(article_params)
       @article.save
       redirect_to article_path(@article)
 
     end
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_path
+    end
+
+    def edit
+    end
+
+    def updata
+    end
+    
 
 end
